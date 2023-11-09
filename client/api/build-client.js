9ac4add597f5f8f4cf0994763b59f043da9c0283
Tim <timournesterov@gmail.com>
2023-11-08 22:22:31 -0500

    disable https

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,9 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     //we are on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      // baseURL:
+      //   "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.eds-emporium.shop/",
       headers: req.headers,
       //   headers: {
       //     host: "ticketing.dev",
