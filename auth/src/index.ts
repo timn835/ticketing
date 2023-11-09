8ce67dcc49a50eda374750de732c3e383859263d
Tim <timournesterov@gmail.com>
2023-11-08 20:12:40 -0500

    test workflow by changing auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -16,7 +16,7 @@ const start = async () => {
     console.error(error);
   }
   app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log("Listening on port 3000!");
   });
 };
 
